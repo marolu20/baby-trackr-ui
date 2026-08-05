c4089ea40a9d030a7662fe1aa85be7cd0cb00dfe
marolu20 <mariamolupitan@gmail.com>
2026-08-05 06:32:27 -0400

    update dashboard name to BabyOverview

---
+++ src/components/baby/BabyOverview.jsx
@@ -0,0 +1,7 @@
+import DashboardGrid from "../../layouts/DashboardGrid.jsx"
+
+export function BabyOverview({dashboard}) {
+    return <DashboardGrid dashboard={dashboard} />;
+}
+
+export default BabyOverview;
